b4eb60fab32c93cfda5b47dcc45dea9cc9ba6a49
ArmandSoto <armand.e.soto@gmail.com>
2019-09-10 10:14:55 -0400

    Changed dir file structure and added new route for tasks api, added id key to tasks.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const path = require('path');
+//consider requiring moment
+const tasks = require('./tasks');
 const app = express();
 const PORT = process.env.port || 5000;
+//check to see if server works
 
-app.get('/', (req, res)=>{
-    res.send('Hello you fucking asshole');
+// (Route) = gets all numbers
+app.get('/api/tasks', (req, res)=>{
+    res.json(tasks);
 });
+
+app.get('/api/tasks/:id', (req, res)=>{
+    res.json(tasks.filter(task => task.id === parseInt(req.params.id)));
+});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//push Create new Task
+
+
+//put Update Task
+
+//delete delete Task
 
 app.listen(PORT, () => console.log(`You are listening on PORT: ${PORT}`));
--- tasks.js
+++ tasks.js
@@ -1,13 +1,16 @@
 const tasks = [
     {
-        name: "",
+        id: 1,
+        name: "Go to Supermarket",
         status: "inactive"
     },
     {
+        id: 2,
         name: "",
         status: "inactive"
     },
-    {
+    {   
+        id: 3,
         name: "",
         status: "inactive"
     }
